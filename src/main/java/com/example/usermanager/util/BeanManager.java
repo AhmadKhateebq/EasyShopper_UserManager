c61280a0f8529bba48c7222627a8be136b4dbb04
AhmadKhateebq <AhmadKhateebq@gmail.com>
2023-05-03 00:55:26 +0300

    deleted unnecessary classes

--- src/main/java/com/example/usermanager/util/BeanManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.usermanager.util;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.stereotype.Component;
-@Component
-public class BeanManager {
-
-}
