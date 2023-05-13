12b72a18db8b43ef447289d58c4e71ff9be0d8f4
AhmadKhateebq <AhmadKhateebq@gmail.com>
2023-05-13 11:48:50 +0300

    encrypt password and matches

--- users_table_creation.sql
+++ users_table_creation.sql
@@ -1,22 +1,16 @@
 create database easy_shopper;
 use easy_shopper;
 CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(15),
-    fname VARCHAR(15),
-    lname VARCHAR(15),
-    email VARCHAR(50)
+                       id INT PRIMARY KEY AUTO_INCREMENT,
+                       username VARCHAR(15),
+                       fname VARCHAR(15),
+                       lname VARCHAR(15),
+                       email VARCHAR(50)
 );
 CREATE TABLE passwords (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    password VARCHAR(25),
-    CONSTRAINT FK_UsersPassword FOREIGN KEY (user_id)
-        REFERENCES users (id)
-);
-CREATE TABLE admins (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    user_id INT,
-    CONSTRAINT FK_UsersAmins FOREIGN KEY (user_id)
-        REFERENCES users (id)
+                           id INT PRIMARY KEY AUTO_INCREMENT,
+                           user_id INT,
+                           password VARCHAR(100),
+                           CONSTRAINT FK_UsersPassword FOREIGN KEY (user_id)
+                               REFERENCES users (id)
 );
