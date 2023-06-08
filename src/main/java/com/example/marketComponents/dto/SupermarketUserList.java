a4e3d0f85bc5890f00a82cd17ce53b7096a101c9
AhmadKhateebq <AhmadKhateebq@gmail.com>
2023-06-08 18:35:08 +0300

    added the supermarket radius search

---
+++ src/main/java/com/example/marketComponents/dto/SupermarketUserList.java
@@ -0,0 +1,18 @@
+package com.example.marketComponents.dto;
+
+import com.example.marketComponents.model.Product;
+import com.example.marketComponents.model.Supermarket;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SupermarketUserList {
+    Supermarket supermarket;
+    List<Product> contains;
+    List<Product> dontContains;
+    double containPercentage;
+}
